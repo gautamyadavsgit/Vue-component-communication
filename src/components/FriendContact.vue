d1d7e1032c2d250d23ce5b4ba9689604f8c780a2
gautamyadavsgit <gautaminit@gmail.com>
2022-11-05 17:23:19 +0530

    validated

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,12 +1,10 @@
 <template>
   <li>
-    <h2>{{ name   }}  {{friendIsFavorite === '1' ? '(Favourite)' : ''}}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favourite)" : "" }}</h2>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
-    <button @click="changeFavorite">
-      Change
-    </button>
+    <button @click="changeFavorite">Change</button>
 
     <ul v-if="detailsAreVisible">
       <li>
@@ -23,12 +21,35 @@
 
 <script>
 export default {
-  props: [
-    'name',
-    'phoneNumber',
-    'email',
-    'isFavorite',
-],
+  //   props: [
+  //     'name',
+  //     'phoneNumber',
+  //     'email',
+  //     'isFavorite',
+  // ],
+
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function (value) {
+        return value === '1' || value === '0';
+      },
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
@@ -38,20 +59,20 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
-      friendIsFavorite:this.isFavorite,
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
-    changeFavorite(){
-      if(this.friendIsFavorite == '1'){
-        this.friendIsFavorite = '0';
-      }else{
-        this.friendIsFavorite = '1';
+    changeFavorite() {
+      if (this.friendIsFavorite == "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
       }
-    }
+    },
   },
 };
 </script>
